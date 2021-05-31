02d8be2aee6de0e6e35cfc6028c5ea1ebfffaf71
Wangzheaos <50896792+Wangzheaos@users.noreply.github.com>
2021-05-31 16:43:27 +0800

    Add files via upload

---
+++ MT-CNV/平均老师/data.py
@@ -0,0 +1,212 @@
+# Copyright (c) 2018, Curious AI Ltd. All rights reserved.
+#
+# This work is licensed under the Creative Commons Attribution-NonCommercial
+# 4.0 International License. To view a copy of this license, visit
+# http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to
+# Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+
+"""Functions to load data from folders and augment it"""
+
+import itertools
+import logging
+import os.path
+
+from PIL import Image
+import numpy as np
+from torch.utils.data.sampler import Sampler
+import torch
+
+LOG = logging.getLogger('main')
+NO_LABEL = -1
+
+
+class data_prefetcher():
+    def __init__(self, loader):
+        self.loader = iter(loader)
+        self.stream = torch.cuda.Stream()
+        # self.mean = torch.tensor([0.485 * 255, 0.456 * 255, 0.406 * 255]).cuda().view(1, 3, 1, 1)
+        # self.std = torch.tensor([0.229 * 255, 0.224 * 255, 0.225 * 255]).cuda().view(1, 3, 1, 1)
+        # With Amp, it isn't necessary to manually convert data to half.
+        # if args.fp16:
+        #     self.mean = self.mean.half()
+        #     self.std = self.std.half()
+        self.preload()
+
+    def preload(self):
+        try:
+            self.next_input, self.next_target = next(self.loader)
+        except StopIteration:
+            self.next_input = None
+            self.next_target = None
+            return
+        with torch.cuda.stream(self.stream):
+            if len(self.next_input) == 2:
+                input_1, input_2 = self.next_input
+                input_1 = input_1.cuda(non_blocking=True)
+                input_2 = input_2.cuda(non_blocking=True)
+                self.next_input = input_1.float(), input_2.float()
+
+            else:
+                self.next_input = self.next_input.cuda(non_blocking=True).float()
+            self.next_target = self.next_target.cuda(non_blocking=True)
+
+    def next(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        input = self.next_input
+        target = self.next_target
+        self.preload()
+        return input, target
+
+
+class RandomTranslateWithReflect:
+    """Translate image randomly
+
+    Translate vertically and horizontally by n pixels where
+    n is integer drawn uniformly independently for each axis
+    from [-max_translation, max_translation].
+
+    Fill the uncovered blank area with reflect padding.
+    """
+
+    def __init__(self, max_translation):
+        self.max_translation = max_translation
+
+    def __call__(self, old_image):
+        xtranslation, ytranslation = np.random.randint(-self.max_translation,
+                                                       self.max_translation + 1,
+                                                       size=2)
+        xpad, ypad = abs(xtranslation), abs(ytranslation)
+        xsize, ysize = old_image.size
+
+        flipped_lr = old_image.transpose(Image.FLIP_LEFT_RIGHT)
+        flipped_tb = old_image.transpose(Image.FLIP_TOP_BOTTOM)
+        flipped_both = old_image.transpose(Image.ROTATE_180)
+
+        new_image = Image.new("RGB", (xsize + 2 * xpad, ysize + 2 * ypad))
+
+        new_image.paste(old_image, (xpad, ypad))
+
+        new_image.paste(flipped_lr, (xpad + xsize - 1, ypad))
+        new_image.paste(flipped_lr, (xpad - xsize + 1, ypad))
+
+        new_image.paste(flipped_tb, (xpad, ypad + ysize - 1))
+        new_image.paste(flipped_tb, (xpad, ypad - ysize + 1))
+
+        new_image.paste(flipped_both, (xpad - xsize + 1, ypad - ysize + 1))
+        new_image.paste(flipped_both, (xpad + xsize - 1, ypad - ysize + 1))
+        new_image.paste(flipped_both, (xpad - xsize + 1, ypad + ysize - 1))
+        new_image.paste(flipped_both, (xpad + xsize - 1, ypad + ysize - 1))
+
+        new_image = new_image.crop((xpad - xtranslation,
+                                    ypad - ytranslation,
+                                    xpad + xsize - xtranslation,
+                                    ypad + ysize - ytranslation))
+
+        return new_image
+
+
+class TransformTwice:
+    def __init__(self, transform):
+        self.transform = transform
+
+    def __call__(self, inp):
+        out1 = self.transform(inp)
+        out2 = self.transform(inp)
+        return out1, out2
+
+
+def relabel_dataset(dataset, labels):
+    unlabeled_idxs = []
+    # t_idxs = np.empty(len(labels), dtype=int)
+    # t0 = 0
+    # t1 = 1
+    # t2 = 2
+    # t3 = 3
+
+    for idx in range(len(dataset.imgs)):
+        path, _ = dataset.imgs[idx]
+        filename = os.path.basename(path)
+        if filename in labels:
+            label_idx = dataset.class_to_idx[labels[filename]]
+            dataset.imgs[idx] = path, label_idx
+            # if label_idx == 0:
+            #     t_idxs[t0] = idx
+            #     t0 = t0 + 4
+            # elif label_idx == 1:
+            #     t_idxs[t1] = idx
+            #     t1 = t1 + 4
+            # elif label_idx == 2:
+            #     t_idxs[t2] = idx
+            #     t2 = t2 + 4
+            # elif label_idx == 3:
+            #     t_idxs[t3] = idx
+            #     t3 = t3 + 4
+            del labels[filename]
+        else:
+            dataset.imgs[idx] = path, NO_LABEL
+            unlabeled_idxs.append(idx)
+
+    if len(labels) != 0:
+        message = "List of unlabeled contains {} unknown files: {}, ..."
+        some_missing = ', '.join(list(labels.keys()))
+        raise LookupError(message.format(len(labels), some_missing))
+
+    # assert len(t_idxs) == len(dataset.imgs) - len(unlabeled_idxs)
+    labeled_idxs = sorted(set(range(len(dataset.imgs))) - set(unlabeled_idxs))
+
+    # labeled_idxs = t_idxs.tolist()
+
+    return labeled_idxs, unlabeled_idxs
+
+
+class TwoStreamBatchSampler(Sampler):
+    """Iterate two sets of indices
+
+    An 'epoch' is one iteration through the primary indices.
+    During the epoch, the secondary indices are iterated through
+    as many times as needed.
+    """
+
+    def __init__(self, primary_indices, secondary_indices, batch_size, secondary_batch_size):
+        self.primary_indices = primary_indices
+        self.secondary_indices = secondary_indices
+        self.secondary_batch_size = secondary_batch_size
+        self.primary_batch_size = batch_size - secondary_batch_size
+
+        assert len(self.primary_indices) >= self.primary_batch_size > 0
+        assert len(self.secondary_indices) >= self.secondary_batch_size > 0
+
+    def __iter__(self):
+        primary_iter = iterate_once(self.primary_indices)
+        secondary_iter = iterate_eternally(self.secondary_indices)
+
+        return (
+            # list(primary_batch) + (np.random.permutation(secondary_batch)).tolist()
+            primary_batch + secondary_batch
+            for (primary_batch, secondary_batch)
+            in zip(grouper(primary_iter, self.primary_batch_size),
+                   grouper(secondary_iter, self.secondary_batch_size))
+        )
+
+    def __len__(self):
+        return len(self.primary_indices) // self.primary_batch_size
+
+
+def iterate_once(iterable):
+    return np.random.permutation(iterable)
+
+
+def iterate_eternally(indices):
+    def infinite_shuffles():
+        while True:
+            yield np.random.permutation(indices)
+            # yield indices
+
+    return itertools.chain.from_iterable(infinite_shuffles())
+
+
+def grouper(iterable, n):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3) --> ABC DEF"
+    args = [iter(iterable)] * n
+    return zip(*args)
